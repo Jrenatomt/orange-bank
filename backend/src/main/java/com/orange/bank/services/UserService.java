bcda4a9ac77c8aba6f519999bdbedd1b2feebb65
Jrenatomt <jrenatomt@gmail.com>
2020-12-30 02:14:21 -0300

    spring security config

--- backend/src/main/java/com/orange/bank/services/UserService.java
+++ backend/src/main/java/com/orange/bank/services/UserService.java
@@ -1,5 +1,32 @@
 package com.orange.bank.services;
 
-public class UserService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.orange.bank.entities.User;
+import com.orange.bank.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = repository.findByEmail(email);
+		if (user == null) {
+			logger.error("email not found: " + email);
+			throw new UsernameNotFoundException("email not found");
+		}
+		logger.info("email found: " + email);
+		return user;
+	}
 }
